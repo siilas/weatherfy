d2fc7f59ba8a9f434948c4ee7915273af5debd05
Silas Maciel Ferreira <s2it_sferreira@uolinc.com>
2019-02-27 16:47:05 -0300

    Adding dockerfile and resources

--- weatherfy-api/src/main/java/com/github/siilas/weatherfy/resource/WeatherfyResource.java
+++ weatherfy-api/src/main/java/com/github/siilas/weatherfy/resource/WeatherfyResource.java
@@ -1,15 +1,21 @@
 package com.github.siilas.weatherfy.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/weatherfy")
+@RequestMapping("/songs")
 public class WeatherfyResource {
 
-    @GetMapping
-    public String teste() {
+    @GetMapping("/city/{city}")
+    public String getByCity(@PathVariable String city) {
+        return "ok";
+    }
+
+    @GetMapping("/latitude/{latitude}/longitude/{longitude}")
+    public String getByLatitudeAndLongitude(@PathVariable String latitude, @PathVariable String longitude) {
         return "ok";
     }
 
